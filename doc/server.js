037c0b451deab82fba5895332c958660b20b1fb3
KY64 <taurus99@protonmail.com>
2020-05-03 18:45:19 +0700

    Change route

--- doc/server.js
+++ doc/server.js
@@ -3,5 +3,5 @@ const express = require('express'),
       swaggerUi = require('swagger-ui-express'),
       swagger = require('./swagger')
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swagger));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swagger));
 app.listen(8888, () => {console.log('Listenting to 8888')})
