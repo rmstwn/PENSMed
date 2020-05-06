3e892a3dab10bcb5c051817b6478ee06334e08af
KY64 <taurus99@protonmail.com>
2020-05-06 22:38:06 +0700

    Add donor page

--- controller/page.js
+++ controller/page.js
@@ -49,8 +49,16 @@ exports.index_page = async (req,res) => {
         try {
             let hash = hashing(session,10),
                 data = await donor.findOne({session: hash})
-                if(data == null)
+                if (data != null) {
+                    res.redirect('/donor')
+                    return
+                } else if(data == null) {
                     data = await hospital.findOne({session: hash})
+                    if (data != null) {
+                        res.redirect('/hospital')
+                        return
+                    }
+                }
 
             if(data == null) {
                 res.clearCookie('session')
@@ -60,7 +68,7 @@ exports.index_page = async (req,res) => {
             res.sendStatus(500)
             return
         }
-    }        
+    }
     res.sendFile(path.resolve(__dirname, '../public/views/index.html'))
 }
 
@@ -89,4 +97,30 @@ exports.edit_page = async (req,res) => {
         }
     }    
     res.sendFile(path.resolve(__dirname, '../public/views/hospital.html'))
+}
+
+exports.donor_page = async (req,res) => {
+    let {logged_in} = req.cookies,
+        {session} = req.signedCookies
+            
+    if (logged_in == undefined || logged_in != 'yes' || session == undefined || !session) {
+        res.redirect('/')
+        return
+    }
+    else if (session != undefined || session) {
+        try {
+            let hash = hashing(session,10),
+                data = await donor.findOne({session: hash})
+
+            if(data == null) {
+                res.redirect('/')
+                return
+            }
+        } catch(err){
+            console.error(err.toString())
+            res.sendStatus(500)
+            return
+        }
+    }    
+    res.sendFile(path.resolve(__dirname, '../public/views/donor.html'))
 }
